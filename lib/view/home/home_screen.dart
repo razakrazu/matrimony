156ddd3bbda2f16a9c4819e1a2a6c1f0e29401d5
razakrazu <razakrazu135@gmail.com>
2026-03-24 10:23:55 +0530

    home screen all working finish

--- lib/view/home/home_screen.dart
+++ lib/view/home/home_screen.dart
@@ -1,4 +1,3 @@
- 
 import 'dart:developer';
 
 import 'package:firebase_auth/firebase_auth.dart';
@@ -14,7 +13,9 @@ class HomeScreen extends StatelessWidget {
   final FirebaseAuth auth = FirebaseAuth.instance;
   final AuthController controller = Get.put(AuthController());
   final UserSarvices sarviceController = UserSarvices();
-  final UserDetailsController  detailsController  = Get.put(UserDetailsController());
+  final UserDetailsController detailsController = Get.put(
+    UserDetailsController(),
+  );
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,163 +23,139 @@ class HomeScreen extends StatelessWidget {
         title: Text('Home'),
         centerTitle: true,
         leading: IconButton(
-          onPressed: ()  {
-             controller.signOut();
-          
-          
+          onPressed: () {
+            controller.signOut();
           },
           icon: Icon(Icons.logout),
         ),
       ),
-      body:Obx(() {
+      body: Obx(() {
+        if (detailsController.detailsIsLoading.value) {
+          return Center(child: CircularProgressIndicator());
+        }
 
-  if (detailsController.detailsIsLoading.value) {
-    return Center(child: CircularProgressIndicator());
-  }
+        if (detailsController.profileList.isEmpty) {
+          return Center(child: Text("No Profiles Found"));
+        }
 
-  if (detailsController.profileList.isEmpty) {
-    return Center(child: Text("No Profiles Found"));
-  }
+        return Padding(
+          padding: const EdgeInsets.all(12),
 
- 
+          child: GridView.builder(
+            scrollDirection: Axis.vertical,
+            shrinkWrap: true,
+            itemCount: detailsController.profileList.length,
 
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 1,
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 12,
+              childAspectRatio: 0.9,
+            ),
+            itemBuilder: (context, index) {
+              var user = detailsController.profileList[index];
 
-  
-     return  Padding(
-            padding: const EdgeInsets.all(12),
-          
-              child: GridView.builder(
-                scrollDirection: Axis.vertical,
-                shrinkWrap: true,
-                itemCount: detailsController.profileList.length,
-              
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 1,
-                  crossAxisSpacing: 10,
-                  mainAxisSpacing: 12,
-                  childAspectRatio: 0.9,
-                  
+              return Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Color.fromARGB(255, 209, 208, 208),
+
+                  // image: const DecorationImage(
+
+                  //   image: AssetImage("lib/assets/photos.avif"),
+                  //   fit: BoxFit.cover,
+                  // ),
                 ),
-                itemBuilder: (context, index) {
-                var user = detailsController.profileList[index];
-              
-              
-              
-                         return      InkWell(
-                          onTap: (){Get.to(UserDetailsScreen(),arguments: user);
-                          log("${user}");
-                          },
-                           child: Container(
-                                               decoration: BoxDecoration(
-                                                 borderRadius: BorderRadius.circular(20),
-                                            color:Color.fromARGB(255, 209, 208, 208),
-                                         
-                                                 // image: const DecorationImage(
-                                         
-                                                 //   image: AssetImage("lib/assets/photos.avif"),
-                                                 //   fit: BoxFit.cover,
-                                                 // ),
-                                               ),
-                                               child: Stack(
-                                                 children: [
-                                                   InkWell(
-                            onTap: () {},
-                            child: Container(
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(20),
-                                gradient: LinearGradient(
-                                  colors: [
-                                    Colors.transparent,
-                                    Colors.black.withOpacity(0.8),
-                                  ],
-                                  begin: Alignment.topCenter,
-                                  end: Alignment.bottomCenter,
-                                ),
-                              ),
+                child: Stack(
+                  children: [
+                    InkWell(
+                      onTap: () {
+                        Get.to(UserDetailsScreen(), arguments: user);
+                        log("${user}");
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          gradient: LinearGradient(
+                            colors: [
+                              Colors.transparent,
+                              Colors.black.withValues(alpha: 0.8),
+                            ],
+                            begin: Alignment.topCenter,
+                            end: Alignment.bottomCenter,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      bottom: 15,
+                      left: 12,
+                      right: 12,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            user.fullName ?? "No Name",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
                             ),
-                                                   ),
-                                                   Positioned(
-                            bottom: 15,
-                            left: 12,
-                            right: 12,
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                 Text(
-                                  user.fullName ?? "No Name",
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                                         
-                                const SizedBox(height: 4),
-                                         
-                                Row(
-                                  children: const [
-                                    Icon(
-                                      Icons.star,
-                                      color: Colors.yellow,
-                                      size: 16,
-                                    ),
-                                    SizedBox(width: 4),
-                                    Text(
-                                      "85%",
-                                      style: TextStyle(color: Colors.white),
-                                    ),
-                                  ],
-                                ),
-                                         
-                                const SizedBox(height: 4),
-                                         
-                                 Text(
-                                  user.job ?? "No job",
-                                  style: TextStyle(
-                                    color: Colors.white70,
-                                    fontSize: 12,
-                                  ),
-                                ),
-                                         
-                                    
-                                      Text("${
-                                      user.residentState ?? "No State"},${
-                                       user.residentCity ?? "No city"}",
-                                      style: TextStyle(
-                                        color: Colors.white70,
-                                        fontSize: 12,
-                                      ),
-                                      ),                            
-                                  
-                              ],
-                            ),
-                                                   ),
-                                         
-                                                   Positioned(
-                            bottom: 15,
-                            right: 12,
-                            child: Container(
-                              padding: const EdgeInsets.all(8),
-                              decoration: const BoxDecoration(
-                                shape: BoxShape.circle,
-                                color: Colors.white,
-                              ),
-                              child: const Icon(Icons.favorite_border, size: 20),
-                            ),
-                                                   ),
-                                                 ],
-                                               ),
-                                             ),
-                         );
-                },
-              ),
-            
-          );
-      
-        }),
-    );       
-}
-     
-    
-}
+                          ),
 
+                          const SizedBox(height: 4),
+
+                          Row(
+                            children: const [
+                              Icon(Icons.star, color: Colors.yellow, size: 16),
+                              SizedBox(width: 4),
+                              Text(
+                                "85%",
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ],
+                          ),
+
+                          const SizedBox(height: 4),
+
+                          Text(
+                            user.job ?? "No job",
+                            style: TextStyle(
+                              color: Colors.white70,
+                              fontSize: 12,
+                            ),
+                          ),
+
+                          Text(
+                            "${user.residentState ?? "No State"},${user.residentCity ?? "No city"}",
+                            style: TextStyle(
+                              color: Colors.white70,
+                              fontSize: 12,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+
+                    Positioned(
+                      bottom: 15,
+                      right: 12,
+                      child: Container(
+                        padding: const EdgeInsets.all(8),
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.white,
+                        ),
+                        child: const Icon(Icons.favorite_border, size: 20),
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            },
+          ),
+        );
+      }),
+    );
+  }
+}
